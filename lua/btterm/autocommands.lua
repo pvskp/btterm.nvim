543fef89eafb9a82a2bdad0b460e5f43b1ae2ba9
pvskp <pvincius14@gmail.com>
2023-12-18 07:45:44 -0300

    Adjust autocmd

--- lua/btterm/autocommands.lua
+++ lua/btterm/autocommands.lua
@@ -4,23 +4,23 @@ M.BTTERM_OPEN_EVENT = "BTTermOpen"
 
 local btterm_augroup = vim.api.nvim_create_augroup("btterm_augroup", { clear = false })
 -- local events = { "BufWinEnter", "TermOpen", "WinEnter" }
-local events = { M.BTTERM_OPEN_EVENT }
+local events = { "User" }
 
 vim.api.nvim_create_autocmd(events, {
 	group = btterm_augroup,
-	pattern = "term://*",
+	pattern = "M.BTTERM_OPEN_EVENT",
 	command = "startinsert",
 })
 
 vim.api.nvim_create_autocmd(events, {
 	group = btterm_augroup,
-	pattern = "term://*",
+	pattern = "M.BTTERM_OPEN_EVENT",
 	command = "set nonumber",
 })
 
 vim.api.nvim_create_autocmd(events, {
 	group = btterm_augroup,
-	pattern = "term://*",
+	pattern = "M.BTTERM_OPEN_EVENT",
 	command = "set norelativenumber",
 })
 
